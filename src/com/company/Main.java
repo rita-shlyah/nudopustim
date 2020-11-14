9a85890d5af66a205ed83eb5c5a965c8c6a0d345
rita-shlyah <rita.shlyah@gmail.com>
2020-11-14 16:08:27 +0300

    i tried

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Ну, допустим, привет, мир");
+	    System.out.println("Say me what am i doing here?");
     }
 }
